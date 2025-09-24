811e5e1c0c95585fa8a052b9a0e4b7f446c5189f
jakemeacham <jake.meacham@capgemini.com>
2025-09-24 13:07:49 +0100

    feat:intiial setup

---
+++ src/test/java/com/transfermarket/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transfermarket.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

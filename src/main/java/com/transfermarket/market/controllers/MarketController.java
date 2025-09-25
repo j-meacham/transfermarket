898b4ac06ce8184d2aba1816d6a184721abc2be4
j-meacham <jake.meacham@capgemini.com>
2025-09-25 13:21:50 +0100

    feat: final set up of springboot and basic service methods

--- src/main/java/com/transfermarket/market/controllers/MarketController.java
+++ src/main/java/com/transfermarket/market/controllers/MarketController.java
@@ -7,35 +7,49 @@ import com.transfermarket.market.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ClubController {
-
-    private Club club;
-
+public class MarketController {
     @Autowired
     private ClubService clubService;
 
+    @Autowired
+    private PlayerService playerService;
+
 
     @Autowired
-    public ClubController() {
+    public MarketController() {
         System.out.println("In constructor: " + getClass().getSimpleName());
-        club = new Club("Nottm Forest", 010020201,10);
     }
 
     @GetMapping("/clubinfo/{clubId}")
     public String getClubInfo(@PathVariable Integer clubId) {
 
-        return ClubService.getClubById(clubId).toString();
+        if (getClubById(clubId) == null) {
+            return "Club not found";
+        } else {
+            return getClubById(clubId).toString();
+        }
 
     }
 
     @GetMapping("/playerinfo/{playerId}")
     public String getPlayerInfo(@PathVariable Integer playerId) {
 
-        return PlayerService.getPlayerById(playerId).toString();
+        if (getPlayerById(playerId) == null) {
+            return "Player not found";
+        } else {
+            return getPlayerById(playerId).toString();
+        }
+    }
+
+    private Players getPlayerById(int playerId) {
+        return playerService.getPlayerById(playerId);
+    }
+
+    private Club getClubById(int clubId) {
+        return clubService.getClubById(clubId);
     }
 
 }
